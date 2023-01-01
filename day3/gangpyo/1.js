b349e21798a3f4109d075b31775f2ff7841e3079
gangpyono <shrkdvy123@gmail.com>
2023-01-01 22:01:48 +0900

    add: day3-1 문제풀이 수정

--- day3/gangpyo/1.js
+++ day3/gangpyo/1.js
@@ -1,2 +1,14 @@
 const datas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const odds = datas.filter((data) => data % 2 !== 0);
+
+class Numbers {
+  #data;
+
+  constructor(data) {
+    this.#data = data;
+  }
+
+  get onlyOdds() {
+    return this.#data.filter((data) => data % 2 !== 0);
+  }
+}
