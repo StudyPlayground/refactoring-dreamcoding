13b3a26002a5b49becd82b869d13d89fc19aff75
gangpyono <shrkdvy123@gmail.com>
2023-01-03 00:59:42 +0900

    fix : 브랜치 내용 수정

--- day3/jaeho/6.js
+++
@@ -1,8 +0,0 @@
-function isAllSame(arr) {
-  const sum = arr.reduce((total, current) => total + current, 0);
-  const sumIfAllSame = arr[0] * arr.length;
-  return sum === sumIfAllSame;
-}
-
-console.log(isAllSame([1, 2, 3, 4, 5]));
-console.log(isAllSame([1, 1, 1, 1, 1]));
