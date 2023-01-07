6aaa2b5f40b09bf70fc714f545157d15ce320e5c
gangpyono <shrkdvy123@gmail.com>
2023-01-07 14:25:55 +0900

    add: day5-3 문제,풀이 추가

---
+++ day5/gangpyo/3.js
@@ -0,0 +1,32 @@
+function orderProduct(name, count, orderTime, requestMessage) {
+  if (!name) {
+    throw new OrderProcessingError("상품명을 입력해주세요.");
+  }
+
+  return {
+    name,
+    count: count ?? 1,
+    orderTime: orderTime ?? new Date(),
+    requestMessage: requestMessage ?? "최대한 빨리 배송해주세요.",
+  };
+}
+
+class OrderProcessingError extends Error {
+  #errorMessage;
+  constructor(errorMessage) {
+    super();
+    this.#errorMessage = errorMessage;
+  }
+
+  printError() {
+    console.log(this.#errorMessage);
+  }
+}
+
+try {
+  orderProduct(undefined, 12, "20220107", ".부재시 문앞에 놔주세요.");
+} catch (error) {
+  if (error instanceof OrderProcessingError) {
+    error.printError();
+  }
+}
