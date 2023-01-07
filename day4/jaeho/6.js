bdded959bbf633b07cb37c262ff5974d6c90bdb5
wogha95 <wogha95@naver.com>
2023-01-07 21:28:31 +0900

    add: day4-6 풀이

---
+++ day4/jaeho/6.js
@@ -0,0 +1,36 @@
+// 관련된 데이터와 로직을 클래스로 묶어 캡슐화하였습니다.
+
+class User {
+  #name;
+  #consumption;
+  #point;
+
+  constructor(user) {
+    this.#name = user.name;
+    this.#consumption = user.consumption;
+    this.#point = user.point;
+  }
+
+  get rank() {
+    if (this.#consumption < User.consumpotionCriteria) {
+      return "C";
+    }
+
+    if (this.#point <= User.pointCriteria) {
+      return "B";
+    }
+
+    return "A";
+  }
+
+  static consumpotionCriteria = 1000;
+  static pointCriteria = 200;
+}
+
+const user1 = new User({ name: "user1", consumption: 10000, point: 500 });
+const user2 = new User({ name: "user2", consumption: 100, point: 10 });
+const user3 = new User({ name: "user3", consumption: 1500, point: 100 });
+
+console.log(user1.rank);
+console.log(user2.rank);
+console.log(user3.rank);
