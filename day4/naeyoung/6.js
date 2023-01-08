ec16c2e8f892ffd87b858e3f696e4beb9c76459e
Naeyoung Kwon <75849590+nyoung113@users.noreply.github.com>
2023-01-08 14:36:04 +0900

    add: ch4-6 문제 추가

---
+++ day4/naeyoung/6.js
@@ -0,0 +1,18 @@
+function checkUsersRank(user) {
+  const { consumption, point } = user;
+  if (consumption < 1000) {
+    return 'C';
+  }
+  if (point <= 200) {
+    return 'B';
+  }
+  return 'A';
+}
+
+const user1 = { name: 'user1', consumption: 10000, point: 500 };
+const user2 = { name: 'user2', consumption: 100, point: 10 };
+const user3 = { name: 'user3', consumption: 1500, point: 100 };
+
+console.log(checkUsersRank(user1));
+console.log(checkUsersRank(user2));
+console.log(checkUsersRank(user3));
