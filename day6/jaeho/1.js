9efb5d177c6cedd366f4dd50eeb0f8442ef2730f
wogha95 <wogha95@naver.com>
2023-01-09 20:39:55 +0900

    add: day6-1,2 문제

---
+++ day6/jaeho/1.js
@@ -0,0 +1,38 @@
+// 위임(컴포지션)을 상속으로 바꾸기
+
+class Dog {
+  speak() {
+    console.log("Bark !!");
+  }
+
+  static create(type) {
+    switch (type) {
+      case "Pomeranian":
+        return new Pomeranian();
+      case "WelshCorgi":
+        return new WelshCorgi();
+      default:
+        return new Dog();
+    }
+  }
+}
+
+class Pomeranian extends Dog {
+  speak() {
+    console.log("Pome ~!");
+  }
+}
+
+class WelshCorgi extends Dog {
+  speak() {
+    console.log("Corgi ~~");
+  }
+}
+
+const pome = Dog.create("Pomeranian");
+const corgi = Dog.create("WelshCorgi");
+const dog = Dog.create();
+
+pome.speak();
+corgi.speak();
+dog.speak();
