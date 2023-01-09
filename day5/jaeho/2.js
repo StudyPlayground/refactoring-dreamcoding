bd66583086ca85268f3a3213db9cfffa6cc2b858
wogha95 <wogha95@naver.com>
2023-01-09 20:39:35 +0900

    fix: day5-2 문제 변경

--- day5/jaeho/2.js
+++ day5/jaeho/2.js
@@ -1,51 +1,32 @@
-// 12.13 서브클래스를 위임으로 바꾸기
+// 11.4 플래그 인수 제거하기
 
-class Camera {
-  #pixel;
-  constructor(pixel) {
-    this.#pixel = pixel;
+class TrafficLight {
+  #status;
+
+  constructor(light) {
+    this.#status = light === "RED" ? false : true;
   }
-  takePicture() {
-    return "찰칵";
+
+  #messageRedToGreen() {
+    console.log("🔴 -> 🟡 -> 🟢 출발하세요.");
+  }
+
+  #messageGreenToRed() {
+    console.log("🟢 -> 🟡 -> 🔴 속도를 줄이세요.");
+  }
+
+  changeLight() {
+    this.#status ? this.#messageGreenToRed() : this.#messageRedToGreen();
+    this.#status = !this.#status;
   }
 }
 
-class Network {
-  #speed;
-  constructor(speed) {
-    this.#speed = speed;
-  }
-}
+const seoulTrafficLight = new TrafficLight("RED");
+const jejuTrafficLight = new TrafficLight("GREEN");
 
-class SmartPhone {
-  #name;
-  #features;
-  constructor(name, features) {
-    this.#name = name;
-    this.#features = features;
-  }
-
-  static createSmartPhone(name, features) {
-    return new SmartPhone(
-      name,
-      features.map((feature) => SmartPhone.getFeature(feature))
-    );
-  }
-  static getFeature(feature) {
-    switch (feature) {
-      case "camera":
-        return new Camera();
-      case "network":
-        return new Network();
-    }
-  }
-}
-
-const cameraNetworkPhone = SmartPhone.createSmartPhone("iphone14", [
-  "camera",
-  "network",
-]);
-const onlyNetworkPhone = SmartPhone.createSmartPhone("Galaxy Z flip3", [
-  "network",
-]);
-const noFeaturePhone = SmartPhone.createSmartPhone("Galaxy Z flip3");
+seoulTrafficLight.changeLight();
+seoulTrafficLight.changeLight();
+seoulTrafficLight.changeLight();
+jejuTrafficLight.changeLight();
+jejuTrafficLight.changeLight();
+jejuTrafficLight.changeLight();
